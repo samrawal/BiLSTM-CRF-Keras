8c95d914701029d74dad2daaddb6857931cbc2a3
samrawal <samrawal@asu.edu>
2018-11-25 22:49:58 -0700

    Wrote gold file error temporary patch

--- src/data/n2c2_utils.py
+++ src/data/n2c2_utils.py
@@ -1,13 +1,17 @@
 
 # coding: utf-8
 
-# In[1]:
+# In[6]:
 
 
-import clamp_parser as clamp
+import os, sys
+PROJ_PATH = open('/tmp/PROJ_PATH.txt', 'r').read().strip()
+sys.path.append(PROJ_PATH+'/src/')
+from utils.word import Word
+import data.clamp_parser as clamp
 
 
-# In[3]:
+# In[20]:
 
 
 class n2c2Parser():    
@@ -54,6 +58,13 @@ class n2c2Parser():
                 end_offsets = [offsets[1], offsets[3]]
                 text = linesplit[5:]
 
+                # TODO: this handles the "Gold File Issue" but in the README
+                if ((int(end_offsets[1]) - int(start_offsets[1]) == 1) and
+                    (int(start_offsets[1]) == int(end_offsets[0]))):
+                    start_offsets = [start_offsets[0]]
+                    end_offsets = [end_offsets[1]]
+                    text = text
+
             for start_offset, end_offset in zip(start_offsets, end_offsets):
                 bundle.append(
                     {
@@ -67,15 +78,29 @@ class n2c2Parser():
         return bundle
 
 
-# In[2]:
+# In[21]:
 
 
-def test():
-    txt_file = '/Volumes/GoogleDrive/My Drive/Colab Notebooks/data/n2c2/track2/training_20180910/100035.txt'
-    ann_file = '/Volumes/GoogleDrive/My Drive/Colab Notebooks/data/n2c2/track2/training_20180910/100035.ann'
-    clamp_file = '/Volumes/GoogleDrive/My Drive/Colab Notebooks/data/n2c2/clamp-parsed/track2-training/output/100035.xmi'
+def test(filenum):
+    txt_file = '/Volumes/GoogleDrive/My Drive/Colab Notebooks/data/n2c2/track2/training_20180910/{0}.txt'.format(filenum)
+    ann_file = '/Volumes/GoogleDrive/My Drive/Colab Notebooks/data/n2c2/track2/training_20180910/{0}.ann'.format(filenum)
+    clamp_file = '/Volumes/GoogleDrive/My Drive/Colab Notebooks/data/n2c2/clamp-parsed/track2-training/output/{0}.xmi'.format(filenum)
 
     parser = n2c2Parser(txt_file)
     parser.parse_file(clamp_file)
     parser.tag_gold_tokens(ann_file)
+
+    return parser
+
+
+# In[25]:
+
+
+if __name__ == '__main__':
+    d = test(110727)
+    tokens = p.tokens
+    x = 12227
+    for t in tokens:
+        if abs(t.start_char - x) < 5:
+            print(t)
 
