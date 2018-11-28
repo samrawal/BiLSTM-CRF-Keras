db8bf5ab9c79a0b15bd2636eb5ad3ff188b00dbe
samrawal <samrawal@asu.edu>
2018-11-28 14:24:30 -0700

    Added IOB support in Word object

--- src/utils/word.py
+++ src/utils/word.py
@@ -7,6 +7,7 @@ class Word():
     pos = None
 
     gold_concept = None
+    iob_tag = None
     gold_text = None
     gold_id = None
     rel_id = None
@@ -16,9 +17,23 @@ class Word():
     def __init__(self):
         pass
 
+    def get_concept_tag(self):
+        if self.gold_concept != None:
+            return '{0}-{1}'.format(self.iob_tag, self.gold_concept)
+        else:
+            self.iob_tag = 'O'
+            return 'O'
+
     def __str__(self):
         string_representation = '{0}//({1}-{2})//{3}'.format(
             self.text, self.start_char, self.end_char, self.gold_concept
         )
         return string_representation
+
+    # set the Word object as a padding token
+    def set_as_pad_token(self):
+        self.start_char = -1
+        self.end_char = -1
+        self.text = 'PADDING_TOKEN'
+        self.gold_concept = 'X'
     
