42ae6fcb8fd513365afe7e8cc1dd5d087db192d7
samrawal <samrawal@asu.edu>
2019-09-22 19:29:33 -0700

    Integrated tracker into train script

--- src/model/train_ner_model.py
+++ src/model/train_ner_model.py
@@ -1,10 +1,12 @@
 import os, sys, pickle, glob, json
+from random import shuffle
 from tqdm import tqdm
 PROJ_PATH = open('/tmp/PROJ_PATH.txt', 'r').read().strip()
 sys.path.append(PROJ_PATH+'/src/')
 
 from utils import preprocessing_utils as pprc
 from utils.word import Word
+from utils.tracker import Tracker
 from model.BiLSTM_CRF_Keras import BiLSTM_CRF
 
 # input: one wordlist (squash multiple if needed)
@@ -13,7 +15,7 @@ def load_data(wordlist, word_tokenizer=None, concept_tokenizer=None,
     padded = pprc.pad_sentences(wordlist)
     sents = [[w.text for w in sent] for sent in padded]
     chars = pprc.split_words(sents, padding=True, pad_len=25)
-    concepts = [[w.gold_concept for w in sent] for sent in padded]
+    concepts = [[w.get_concept_tag() for w in sent] for sent in padded]
 
     tokenized_words, word_tokenizer = pprc.tokenize(sents, t=word_tokenizer)
     tokenized_chars, char_tokenizer = pprc.tokenize_chars(chars)
@@ -24,26 +26,60 @@ def load_data(wordlist, word_tokenizer=None, concept_tokenizer=None,
             (one_hot_concepts, concept_tokenizer))
     
 
-train_wl_path = PROJ_PATH + '/data/n2c2/processed/100035.ser'
-valid_wl_path = PROJ_PATH + '/data/n2c2/processed/100039.ser'
-test_wl_path = PROJ_PATH + '/data/n2c2/processed/100187.ser'
+def combine_wordlists(wordlists_path):
+    wl_paths = glob.glob(wordlists_path + '/*.ser')
+    master_wordlist = []
+    for wl in tqdm(wl_paths, 'Combining wordlists'):
+        master_wordlist.extend(pickle.load(open(wl, 'rb')))
+    return master_wordlist
+    
 
-train_data = pickle.load(open(train_wl_path, 'rb'))
-valid_data = pickle.load(open(valid_wl_path, 'rb'))
-test_data = pickle.load(open(test_wl_path, 'rb'))
-
-(train_sents, word_tokenizer),  (train_chars, char_tokenizer), (train_concept, concept_tokenizer) = load_data(train_data)
-(valid_sents, _),  (valid_chars, _), (valid_concept, _)  = load_data(valid_data, word_tokenizer, concept_tokenizer)
-(test_sents, _),  (test_chars, _), (test_concept, _) = load_data(test_data, word_tokenizer, concept_tokenizer)
+def train_ner_model(wordlists_path):
+    master = combine_wordlists(PROJ_PATH + '/' + wordlists_path)
+    train_valid_split = int(len(master) * 0.8)
+    train_data = master[0 : train_valid_split]
+    valid_data = master[train_valid_split : ]
 
 
-model_parameters = json.load(open(PROJ_PATH + '/src/model/config.json', 'r'))
-model_parameters['vocab_size'] = len(word_tokenizer.word_index)
-model_parameters['char_dim'] = len(char_tokenizer)
-model_parameters['output_dim'] = len(concept_tokenizer.word_index)
+    (train_sents, word_tokenizer),  (train_chars, char_tokenizer), (train_concept, concept_tokenizer) = load_data(train_data)
+    (valid_sents, _),  (valid_chars, _), (valid_concept, _)  = load_data(valid_data, word_tokenizer, concept_tokenizer)
+    #(test_sents, _),  (test_chars, _), (test_concept, _) = load_data(test_data, word_tokenizer, concept_tokenizer)
 
-print(model_parameters)
-model = BiLSTM_CRF(model_parameters)
-model.define_model()
-model.generate_model_diagram()
-model.train(train_sents, train_chars, train_concept, valid_sents, valid_chars, valid_concept)
+
+    config = json.load(open(PROJ_PATH + '/src/model/config.json', 'r'))
+
+    model_parameters = config['parameters']
+    model_parameters['vocab_size'] = len(word_tokenizer.word_index)
+    model_parameters['char_dim'] = len(char_tokenizer)
+    model_parameters['output_dim'] = len(concept_tokenizer.word_index)
+
+    model_info = config['model_info']
+    tracker = Tracker(basedir=PROJ_PATH + '/models/',
+                      desc=model_info['model_description'],
+                      title=model_info['model_title'],
+                      enter_desc=False,
+                      name=model_info['username'],
+    )
+
+    model = BiLSTM_CRF(model_parameters)
+
+    model.define_model(
+        char_embedding_dim = model_parameters['char_embedding_dim'],
+        word_embedding_dim = model_parameters['word_embedding_dim'],
+        char_lstm_cell = model_parameters['char_lstm_cell'],
+        lstm_cell = model_parameters['lstm_cell'],
+    )
+
+    model.generate_model_diagram(tracker.get_model_dir(), tracker.title)
+    model.train(train_sents, train_chars, train_concept, valid_sents, valid_chars, valid_concept)
+    model.save_model(tracker.get_model_dir(), tracker.title)
+
+    # save tokenizers
+    pickle.dump(word_tokenizer,
+                open(tracker.get_model_dir() + '/word_tokenizer.ser', 'wb'))
+    pickle.dump(char_tokenizer,
+                open(tracker.get_model_dir() + '/char_tokenizer.ser', 'wb'))
+    pickle.dump(concept_tokenizer,
+                open(tracker.get_model_dir() + '/concept_tokenizer.ser', 'wb'))
+
+    tracker.log()
